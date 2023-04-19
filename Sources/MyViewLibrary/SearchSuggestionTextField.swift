2131f13826e7158709fb6162cc35fc9fa6a19ba0
杨林青 <yang-linqing@outlook.com>
2023-04-19 12:55:28 +0800

    改进建议文本框的全屏输入体验
    当希望使用单独页面的建议文本框时，可以使用 SearchSuggestionTextFieldButton
    打开一个单独的页面，当选择建议或者完成输入时自动关闭页面。

--- Sources/MyViewLibrary/SearchSuggestionTextField.swift
+++ Sources/MyViewLibrary/SearchSuggestionTextField.swift
@@ -89,28 +89,60 @@ public struct SearchSuggestionTextField: View {
 }
 
 public struct SearchSuggestionTextPage: View {
-    var label: String
     @Binding var text: String
     var suggestions: [String]
     
-    public init(_ label: String, text: Binding<String>, suggestions: [String]) {
-        self.label = label
+    public init(text: Binding<String>, suggestions: [String]) {
         self._text = text
         self.suggestions = suggestions
     }
     
     @State private var editingText = ""
+    @FocusState private var focused: Bool
+    @Environment(\.dismiss) private var dismiss
     
     public var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text("建议")
+                .bold()
+            if suggestions.isEmpty {
+                Text("没有建议内容，请在下方直接输入。")
+            }
+            ScrollView {
+                FlowHStack {
+                    ForEach(suggestions, id: \.self) { suggest in
+                        if suggest != "" {
+                            if text == suggest {
+                                Button(suggest) {
+                                    setText(to: suggest)
+                                }
+                                .buttonStyle(.borderedProminent)
+                                .buttonBorderShape(.capsule)
+                            } else {
+                                Button(suggest) {
+                                    setText(to: suggest)
+                                }
+                                .buttonStyle(.bordered)
+                                .buttonBorderShape(.capsule)
+                                .tint(.primary)
+                            }
+                        }
+                    }
+                }
+            }
+            Text("其他")
+                .bold()
             HStack {
                 TextField("", text: $editingText)
+                    .focused($focused)
                     .onSubmit {
                         text = editingText
+                        dismiss()
                 }
                 Button {
                     text = ""
                     editingText = ""
+                    focused = true
                 } label: {
                     Image(systemName: "xmark.circle.fill")
                 }.tint(.secondary)
@@ -121,33 +153,58 @@ public struct SearchSuggestionTextPage: View {
                 Color(uiColor: .secondarySystemGroupedBackground)
             }
             .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
-            .padding(.horizontal)
-            ScrollView {
-                VStack(alignment: .leading) {
-                    ForEach(suggestions, id: \.self) { suggest in
-                        if suggest != "" {
-                            Button(suggest) {
-                                text = suggest
-                                editingText = suggest
-                            }
-                            .buttonStyle(.bordered)
-                            .buttonBorderShape(.capsule)
-                            .tint(.primary)
-                        }
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal, 32.0)
-            }
         }
+        .padding()
         .background {
             Color(uiColor: .systemGroupedBackground)
                 .ignoresSafeArea()
         }
-        .navigationTitle(label)
         .onAppear {
             editingText = text
         }
+        .interactiveDismissDisabled(focused)
+    }
+    
+    private func setText(to newValue: String) {
+        text = newValue
+        editingText = newValue
+        focused = false
+        dismiss()
+    }
+}
+
+public struct SearchSuggestionTextFieldButton: View {
+    var label: String?
+    @Binding var text: String
+    var suggestions: [String]
+    
+    public init(_ label: String? = nil,
+                text: Binding<String>,
+                suggestions: [String]
+    ) {
+        self.label = label
+        self._text = text
+        self.suggestions = suggestions
+    }
+    
+    @State private var isEditing = false
+    
+    public var body: some View {
+        Button {
+            isEditing = true
+        } label: {
+            HStack {
+                if label != nil {
+                    Text(label!)
+                    Spacer()
+                }
+                Text(text)
+            }
+        }
+        .tint(.primary)
+        .sheet(isPresented: $isEditing, content: {
+            SearchSuggestionTextPage(text: $text, suggestions: suggestions)
+        })
     }
 }
 
@@ -158,7 +215,9 @@ struct SearchSuggestionTextField_Previews: PreviewProvider {
     static var previews: some View {
         NavigationStack {
             Form {
-                SearchSuggestionTextField("SearchText", text: $text, suggestions: suggestions)
+                SearchSuggestionTextFieldButton("SearchText", text: $text, suggestions: suggestions)
+                SearchSuggestionTextFieldButton(text: $text, suggestions: suggestions)
+                SearchSuggestionTextFieldButton("无建议文本框", text: $text, suggestions: [])
             }
             .navigationTitle("Form")
             //SearchSuggestionTextPage("菜品", text: $text, suggestions: suggestions)
